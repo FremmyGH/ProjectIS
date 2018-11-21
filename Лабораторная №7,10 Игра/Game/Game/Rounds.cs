12423e4bdb5c4056262644208c22a163e8b37e96
FremmyGH <38381533+FremmyGH@users.noreply.github.com>
2018-11-21 16:42:34 +0500

    Добавил 11 лабораторную, переделал игру и 8 лабораторную

--- Лабораторная №7,10 Игра/Game/Game/Rounds.cs
+++ Лабораторная №7,10 Игра/Game/Game/Rounds.cs
@@ -26,16 +26,23 @@ namespace Game
             var connection = @"Data Source=DESKTOP-FPQ69BF\SQLSERVER;Initial Catalog=GameDice;Integrated Security=True";
             for (var i = 0; i < Players.Length(); i++)
             {
-                var checkQuery = $"SELECT Name From Player WHERE Name='{Players[i].Name}'";
+                var checkQuery = $"SELECT Name, Score From Player WHERE Name='{Players[i].Name}'";
+                var scoreQuery = $"SELECT Score, Score From Player WHERE Name='{Players[i].Name}'";
+
                 var insertQuery = $"INSERT INTO Player(Name,Score) VALUES('{Players[i].Name}','{Players[i].SumMoney}')";
+                
                 using (var con = new SqlConnection(connection))
                 {
                     con.Open();
                     var cmd = new SqlCommand(checkQuery, con);
+                    var cmdScore = new SqlCommand(scoreQuery, con);
                     var res = cmd.ExecuteScalar();
+                    var resScore = cmdScore.ExecuteScalar();
                     if (res != null)
                     {
-                        var updateQuery = $"UPDATE Player SET Score='{Players[i].SumMoney}'WHERE Name='{Players[i].Name}'";
+                        var resultMoney = Convert.ToInt32(resScore);
+                        resultMoney += Convert.ToInt32(Players[i].SumMoney);
+                        var updateQuery = $"UPDATE Player SET Score='{resultMoney}'WHERE Name='{Players[i].Name}'";
                         var cmdUpd = new SqlCommand(updateQuery, con);
                         cmdUpd.ExecuteNonQuery();
                     }
